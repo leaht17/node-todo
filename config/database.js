f7bfd8fb44a6c5780aa34173aa64605a39d0351b
Leah <41168640+leaht17@users.noreply.github.com>
2021-07-22 14:21:02 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,2 +1,1 @@
 exports.connection = process.env.MONGO_URL || 'mongodb://localhost/todo'
-// exports.connection = process.env.MONGO_URL || 'mongodb://leaht17:vJdh8288ppjTw3J7xnsr9yPze8AtSm2jaNp2b7prdmgG9Kegs3tjeh35RCKV1Xoo9V5qh17WUkFTmmMMOMko9Q==@leaht17.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@leaht17@'
